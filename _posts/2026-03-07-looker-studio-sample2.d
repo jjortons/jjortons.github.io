f0012fad82911576ab8f0b9107222032026e7954
jjortons <118932716+jjortons@users.noreply.github.com>
2026-03-07 10:39:38 -0700

    Create 2026-03-07-looker-studio-sample2.d

---
+++ _posts/2026-03-07-looker-studio-sample2.d
@@ -0,0 +1,12 @@
+---
+layout: post
+title: "Looker Studio Sample #2"
+date: 2026-03-07 0:39:09-07:00
+categories: [DataViz]
+---
+
+# Hello!
+Below is a live connection to my AdventureWorks database on Neon.tech. 
+
+
+<iframe width="100%" height="900px" src="https://lookerstudio.google.com/embed/reporting/4fa669ce-beb2-4e14-9834-1f69f2aca7b4/page/KnQrF" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
